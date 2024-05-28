13d69a4bedd2ca0d86c17b5f8d0c994d9f7fdc74
Benj Rich <github@brich.one>
2024-05-28 14:54:51 -0600

    Create venmo_get_token.py

---
+++ src/venmo_get_token.py
@@ -0,0 +1,6 @@
+from venmo_api import Client
+
+access_token = Client.get_access_token(username = '<venmo_username',
+                                       password = '<venmo_password>')
+
+print("My token:", access_token)
